dc0c1b501387c976bdd808004f1c8ce1b8d77128
WarL0ck <main@warl0ck.net>
2018-05-04 21:50:48 +0700

    Initial commit

---
+++ src/main/java/net/warl0ck/firstservice/Person.java
@@ -0,0 +1,41 @@
+package net.warl0ck.firstservice;
+
+public class Person {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
